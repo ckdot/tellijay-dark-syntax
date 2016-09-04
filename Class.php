1e08cfe36df26e56d371bec4c1d153e9d87a9a33
Christian Kilb <christian.kilb@live.com>
2016-09-04 10:31:03 +0200

    initial commit

---
+++ Class.php
@@ -0,0 +1,29 @@
+<?php
+
+class Flasche extends Horst implements Bla
+{
+    public function hello($bla)
+    {
+        // bllaa
+        echo 'blabla';
+        print('hello');
+
+        $array = [
+            'aa', 'bb'
+        ];
+
+        array_values(['aa']);
+
+        return $this->horst($hello, $vlaues['price']);
+    }
+
+    /**
+     * Horsti
+     * @param [type] $hh [description]
+     * @return [type] [description]
+     */
+    public function horst($hh)
+    {
+        echo $hh;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# tellijay-dark-syntax theme
+
+An atom syntax theme similar to the Darcula theme of IntelliJ Idea 15.
+
+![A screenshot of your theme](https://f.cloud.github.com/assets/69169/2289498/4c3cb0ec-a009-11e3-8dbd-077ee11741e5.gif)
